981072673fa647604ce5017010acb04bf6eb5afa
SAFA IDAM HAMED <116453067+SAFAIDAM@users.noreply.github.com>
2023-07-10 22:49:37 +0100

    finall

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
-const cursorDot = document.querySelector("[data-cursor-dot]");
-const cursorOutline = document.querySelector("[data-cursor-outline]");
+document.addEventListener("DOMContentLoaded", function() {
 
-window.addEventListener("mousemove", function (e){
+  const cursorDot = document.querySelector("[data-cursor-dot]");
+  const cursorOutline = document.querySelector("[data-cursor-outline]");
 
-  const posX = e.clientX;
-  const posY = e.clientY;
+  window.addEventListener("mousemove", function (e){
 
-  cursorDot.style.left = `${posX}px`;
-  cursorDot.style.top = `${posY}px`;
+    const posX = e.clientX;
+    const posY = e.clientY;
 
-  cursorOutline.animate({
-    left: `${posX}px`,
-    top: `${posY}px`
-  }, {duration: 500, fill: "forwards"});
+    cursorDot.style.left = `${posX}px`;
+    cursorDot.style.top = `${posY}px`;
+
+    cursorOutline.animate({
+      left: `${posX}px`,
+      top: `${posY}px`
+    }, {duration: 500, fill: "forwards"});
+  });
+  
 });
